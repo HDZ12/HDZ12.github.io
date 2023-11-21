3197d5c080a05142235b3c09a3997348d0d38df3
你的长夏 <99587726+HDZ12@users.noreply.github.com>
2023-11-21 12:47:12 +0000

    Add files via upload

---
+++ table_header.js
@@ -0,0 +1,26 @@
+document.write("<table width=\"650\" border=\"0\" height=\"184\">");
+document.write("<tbody>");
+document.write("<tr>");
+document.write("<td> <img src=\"Images/Donghui _zhao.jpg\" alt=\"Xinyu Chen\"");
+document.write("width=\"162.6\" height=\"200\"> <\/td>");
+document.write("<td>");
+document.write("<h1>赵东辉</h1>");
+document.write("<br>");
+document.write("<b>数据科学与大数据技术</b>");
+document.write("<br>");
+
+document.write("<a href=\"https:\/\/www.dlmu.edu.cn\">大连海事大学<\/a></b>");
+
+document.write("<br>");
+document.write("<br>");
+document.write("<b>联系方式：</b><br>");
+document.write(" 辽宁省 沈阳市 和平区 <br>");
+document.write("E-mail: 2219479402@qq.com <br>");
+document.write("<br>");
+document.write("&nbsp;");
+document.write("<a href=\"https:\/\/github.com\/HDZ12\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<a href=\"https:\/\/www.zhihu.com\/people\/hdz12\/posts\"> <img src=\"Images\/zhihu.png\" alt=\"Zhihu\"  border=\"0\" height=\"25\"><\/a> &nbsp");
+document.write("<\/td>");
+document.write("<\/tr>");
+document.write("<\/tbody>");
+document.write("<\/table>");
